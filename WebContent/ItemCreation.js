9b03a0f68dac9bd3a096966a8f40d596154b3e87
isaakay111 <kayode_ogunlolu@yahoo.com>
2014-07-11 15:09:59 +0300

    Item creation page version 1, js page not functional yet

---
+++ WebContent/ItemCreation.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	
+$(".ratingScale").hide();	
+	
+//function checkRatingScale() {
+//	if(("#rateID").prop("checked"))
+//		$(".ratingScale").show();
+//	else
+//		$("#ratingScale").hide();
+//}	
+//	
+});
