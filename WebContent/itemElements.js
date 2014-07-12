9b89e7722c7b0a5a2bc20478ed9f220363e518db
touzbi <tolbimine@gmail.com>
2014-07-13 02:57:53 +0300

    Refactoring ui

---
+++ WebContent/itemElements.js
@@ -0,0 +1,46 @@
+var ItemElements = {
+	itemName : document.getElementById('itemName'),
+	itemDescription : document.getElementById('itemDescription'),
+	ratingEnabled : document.getElementById('ratingEnabled'),
+	itemToDelete : document.getElementById('itemToDelete'),
+	fbsItem : document.getElementById('FBSItem'),
+	itemToEdit : document.getElementById('itemToEdit'),
+
+	init : function() {
+		itemName.value = 'item';
+		itemDescription.value = 'item';
+	},
+
+	updateItemElement : function(element, items) {
+		element.empty();
+		$.each(items, function(index, item) {
+			element.append('Item: ' + '<span id="item' + (index + 1) + '">'
+					+ item.id + ' ' + item.name + ' ' + item.description + ' '
+					+ item.ratingEnabled + ' ' + item.state + '</span>'
+					+ '<br/>');
+		});
+	},
+
+	updateItemList : function() {
+		$.getJSON('http://localhost:8080/Feedback/rest/items', function(data) {
+			ItemElements.updateItemElement($('#items'), data);
+		});
+	},
+
+	createItem : function(data, next) {
+		$.post('http://localhost:8080/Feedback/rest/items/', data, next);
+	},
+
+	deleteItem : function(itemID, next) {
+		$.ajax({
+			url : 'http://localhost:8080/Feedback/rest/items/' + itemID,
+			type : 'DELETE',
+			success : next
+		});
+	},
+
+	createFeedbackSession : function() {
+		$.post('http://localhost:8080/Feedback/rest/items/' + fbsItem.value
+				+ '/sessions');
+	}
+};
--- WebContent/main.js
+++ WebContent/main.js
@@ -1,79 +1,21 @@
-var itemElements = {
-	itemName : document.getElementById('itemName'),
-	itemDescription : document.getElementById('itemDescription'),
-	ratingEnabled : document.getElementById('ratingEnabled'),
-	itemToDelete : document.getElementById('itemToDelete'),
-	fbsItem : document.getElementById('FBSItem'),
-	itemToEdit : document.getElementById('itemToEdit'),
-};
-
-function init() {
-	itemElements.itemName.value = 'item';
-	itemElements.itemDescription.value = 'item';
-}
-
-function updateItemList() {
-	$.getJSON('http://localhost:8080/Feedback/rest/items', function(data) {
-		$('#items').empty();
-		$.each(data, function(index, item) {
-			$('#items').append(
-					'Item: ' + item.id + ' ' + item.name + ' '
-							+ item.description + ' ' + item.ratingEnabled + ' '
-							+ item.state + ' ' + '<br/>');
-		});
-	});
-}
-
-function createItem(data, next) {
-	$.post('http://localhost:8080/Feedback/rest/items/', data, next);
-}
-
-function deleteItem(itemID, next) {
-	$.ajax({
-		url : 'http://localhost:8080/Feedback/rest/items/' + itemID,
-		type : 'DELETE',
-		success : next
-	});
-}
-
-var goCreateFeedbackSession = function() {
-	$.post('http://localhost:8080/Feedback/rest/items/'
-			+ itemElements.fbsItem.value + '/sessions');
-};
-
-var goDelete = function() {
-	var itemID = itemElements.itemToDelete.value;
-	deleteItem(itemID, updateItemList);
-};
-
-var goEdit = function() {
-	var itemToEdit = itemElements.itemToEdit.value;
-	var ratingEnabled = itemElements.ratingEnabled.checked;
-
-	var data = {
-		toEnable : ratingEnabled
-	};
-
-	$.post(
-			'http://localhost:8080/Feedback/rest/items/' + itemToEdit
-					+ '/rating', data, updateItemList);
-};
-
-var go = function() {
-
-	var data = {
-		itemName : itemElements.itemName.value,
-		itemDescription : itemElements.itemDescription.value,
-	};
-
-	createItem(data, updateItemList);
-};
-
 $(document).ready(function() {
-	init();
-	updateItemList();
-	$('#go').click(go);
-	$('#delete').click(goDelete);
-	$('#newFBSButton').click(goCreateFeedbackSession);
-	$('#editButton').click(goEdit);
+	ItemElements.init();
+	ItemElements.updateItemList();
+
+	$('#go').click(function() {
+
+		var data = {
+			itemName : ItemElements.itemName.value,
+			itemDescription : ItemElements.itemDescription.value,
+		};
+
+		ItemElements.createItem(data, ItemElements.updateItemList);
+	});
+
+	$('#delete').click(function() {
+		var itemID = ItemElements.itemToDelete.value;
+		ItemElements.deleteItem(itemID, ItemElements.updateItemList);
+	});
+
+	$('#newFBSButton').click(ItemElements.createFeedbackSession);
 });
