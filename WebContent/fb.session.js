d061a8b125246e9371067759247d70ab91f0962c
touzbi <tolbimine@gmail.com>
2014-07-14 15:55:43 +0300

    Added session data display

---
+++ WebContent/fb.session.js
@@ -0,0 +1,19 @@
+fb.session = {};
+fb.session.ajax = {};
+
+fb.session.ajax.updateCurrentSessionData = function(itemID) {
+	$.getJSON(fb.host + '/Feedback/rest/items/' + itemID + '/sessions/current',
+			function(fbs) {
+				var fbUnits = fbs.feedbackUnits;
+
+				$.each(fbUnits, function(index, fbu) {
+					$('#fbu').append(fbu.value + '<br/>');
+				});
+			});
+};
+
+$(document).ready(function() {
+	ajax = fb.session.ajax;
+
+	ajax.updateCurrentSessionData(1);
+});
