d8a76c43ae52bf065c9311d15347d3f47e38a51f
touzbi <tolbimine@gmail.com>
2014-07-18 15:50:05 +0300

    Refactoring servlet

--- src/com/feedback/servlet/HomeServlet.java
+++ src/com/feedback/servlet/HomeServlet.java
@@ -1,6 +1,7 @@
 package com.feedback.servlet;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -14,13 +15,15 @@ import javax.servlet.http.HttpServletResponse;
 public class HomeServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
+	private static final String HOME_PAGE = "/ItemCreation.html";
+
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
 	 *      response)
 	 */
 	protected void doGet(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
-		getServletContext().getRequestDispatcher("/ItemCreation.html").forward(
-				request, response);
+		getServletContext().getRequestDispatcher(HOME_PAGE).forward(request,
+				response);
 	}
 }
