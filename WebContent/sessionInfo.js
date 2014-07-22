4adf401f756b2c5a30b13c3fb1c6e32975e805ec
touzbi <tolbimine@gmail.com>
2014-07-22 19:26:03 +0300

    Now displays current session info by default

--- WebContent/sessionInfo.js
+++ WebContent/sessionInfo.js
@@ -2,10 +2,15 @@ $(document)
 		.ready(
 				function() {
 					var ajax = fb.session.ajax;
-					var itemID = fb.getQueryParam('itemID');
-					var sessionIndex = fb.getQueryParam('sessionIndex');
+					var itemIDParam = fb.getQueryParam('itemID');
+					var sessionIndexParam = fb.getQueryParam('sessionIndex');
 
 					var updateAll = function() {
+						var itemID = itemIDParam;
+						var sessionIndex = sessionIndexParam !== null ? sessionIndexParam
+								: 'current';
+						console.log(sessionIndex);
+
 						ajax.updateCurrentSessionData(itemID, sessionIndex,
 								$('#listDataView'),
 								fb.session.dataView.rawDataView);
