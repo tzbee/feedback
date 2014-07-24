d8007a7e0e8d11324a0c722a18c98908f9be0642
touzbi <tolbimine@gmail.com>
2014-07-25 00:33:00 +0300

    Added popup functions and styling (To improve)

--- WebContent/fb.js
+++ WebContent/fb.js
@@ -13,3 +13,58 @@ fb.getQueryParam = function(key) {
 			.match(new RegExp("[?&]" + key + "=([^&]+)(&|$)"));
 	return match && decodeURIComponent(match[1].replace(/\+/g, " "));
 };
+
+/**
+ * Pop up creation and animation functions
+ */
+
+/**
+ * Create a popup element
+ * 
+ * @param text
+ *            content of the popup
+ */
+fb.createPopupWindow = function(text) {
+	$('body').append($('<div>', {
+		html : $('<span>', {
+			class : 'popupText',
+			html : text
+		}),
+		class : 'popup'
+	}));
+};
+
+/**
+ * Show an element and hide it after a while
+ * 
+ * @param element
+ *            the element to show
+ * 
+ * @param speed
+ *            speed of the animation
+ * 
+ * @param duration
+ *            the time the element is shown
+ */
+fb.showPopup = function(element, speed, duration) {
+	element.fadeIn(speed, function() {
+		setTimeout(function() {
+			fb.hidePopup(element, speed);
+		}, duration);
+	});
+};
+
+/**
+ * Hide an element
+ * 
+ * @param element
+ *            the element to hide
+ * 
+ * @param speed
+ *            the speed of the animation
+ * 
+ */
+fb.hidePopup = function(element, speed) {
+	element.fadeOut(speed, function() {
+	});
+};
