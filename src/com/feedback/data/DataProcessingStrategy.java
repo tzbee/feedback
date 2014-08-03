b0b6fa7e8a9d8c60b51e1c78529cae78c9110a58
touzbi <tolbimine@gmail.com>
2014-08-03 13:45:43 +0300

    Commenting java

--- src/com/feedback/data/DataProcessingStrategy.java
+++ src/com/feedback/data/DataProcessingStrategy.java
@@ -1,5 +1,16 @@
 package com.feedback.data;
 
+/**
+ * Strategy interface used to process data
+ */
 public interface DataProcessingStrategy {
+
+	/**
+	 * Transforms original data by applying calculations to it
+	 * 
+	 * @param data
+	 *            the original data to process
+	 * @return the processed data
+	 */
 	Data process(Data data);
 }
