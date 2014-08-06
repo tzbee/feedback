fa4e351903d32410e5078f6517e6b606dd7e1c70
touzbi <tolbimine@gmail.com>
2014-08-06 18:51:13 +0300

    Commmenting js

--- WebContent/resources/js/fb.account.js
+++ WebContent/resources/js/fb.account.js
@@ -10,8 +10,15 @@ fb.account = {};
 
 (function($, document, window) {
 
+	/**
+	 * Create a new account key
+	 * 
+	 * @param mail
+	 *            mail address identifying the account
+	 * @param accountType
+	 *            account type (user, owner)
+	 */
 	fb.account.createNewKey = function(mail, accountType) {
 		console.log("Key " + mail + " " + accountType + " created");
 	};
-
 }(jQuery, document, window));
