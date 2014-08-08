890bbc2a369ed8c067b102f3f4b2f97c1f100d80
touzbi <tolbimine@gmail.com>
2014-08-08 15:06:03 +0300

    merged fb.account and fb.js

--- WebContent/fb.js
+++ WebContent/fb.js
@@ -690,4 +690,72 @@ fb.session.dataView = {};
 				});
 	};
 
+	/**
+	 * ACCOUNT OPERATIONS
+	 */
+
+	fb.account = {};
+
+	/**
+	 * Create a new account key
+	 * 
+	 * @param mail
+	 *            mail address identifying the account
+	 * @param accountType
+	 *            account type (user, owner)
+	 * 
+	 * @param keyContainerElement
+	 *            element to display the generated key in
+	 */
+	fb.account.createNewKey = function(mail, accountType, keyContainerElement) {
+
+		// Rest URL to post on
+		var url = 'rest/users/key';
+
+		// Data to send
+		var postData = {
+			mail : mail,
+			accountType : accountType
+		};
+
+		// Ajax post
+		$.post(url, postData, function(responseData) {
+			keyContainerElement.empty();
+
+			var accountKey = responseData;
+
+			// Create the link
+			keyContainerElement.append($('<a>').attr('href', '#').html(
+					accountKey).attr('id', 'keyLink').click(function() {
+
+				// Create the account on click
+				fb.account.createAccount(accountKey);
+
+				// Notify user
+				fb.notification('New key created', 'info');
+			}));
+		});
+	};
+
+	/**
+	 * Create a new account based on the account key
+	 * 
+	 * @param accountKey
+	 *            the key to use to create the account
+	 */
+	fb.account.createAccount = function(accountKey) {
+
+		// Rest URL to post on
+		var url = 'rest/users/';
+
+		// Data to send
+		var postData = {
+			userKey : accountKey
+		};
+
+		// Ajax post
+		$.post(url, postData);
+
+	};
+
 })(jQuery, window, document);
