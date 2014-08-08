1d872b3e7d331d79adc0b46ec2f723011f6b73c8
touzbi <tolbimine@gmail.com>
2014-08-08 14:48:32 +0300

    Merged css files into one

--- src/com/feedback/rest/UserResource.java
+++ src/com/feedback/rest/UserResource.java
@@ -6,9 +6,7 @@ import javax.ws.rs.FormParam;
 import javax.ws.rs.NotAuthorizedException;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 import com.feedback.beans.User;
