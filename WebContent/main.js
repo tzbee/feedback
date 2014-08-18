dbb6c0e5a94c88bb3a8c332df044da474018f54f
touzbi <tolbimine@gmail.com>
2014-08-18 11:10:25 +0300

    Added login page link

--- WebContent/main.js
+++ WebContent/main.js
@@ -1,27 +1,33 @@
 /*
  * Collation of onclick functions
  */
-$(document).ready(function() {
+$(document)
+		.ready(
+				function() {
 
-	$("#home").click(function() {
+					$("#home").click(function() {
 
-		window.location.href = 'home';
+						window.location.href = 'home';
 
-	});
+					});
 
-	$("#rateItemList").click(function() {
+					$("#rateItemList").click(function() {
 
-		window.location.href = 'itemList.html';
+						window.location.href = 'itemList.html';
 
-	});
+					});
 
-	$("#itemList").click(function() {
+					$("#itemList").click(function() {
 
-		window.location.href = 'ItemCreation.html';
+						window.location.href = 'ItemCreation.html';
 
-	});
+					});
 
-	$("#loginButton").click(function() {
-		window.location.href = 'login.html';
-	});
-});
+					var loginButton = '</label> <span>&nbsp;</span><span>&nbsp;</span> <label> <span>&nbsp;</span><input type="button" id="loginButton" class="loginButton" value="Login" />';
+
+					$('.testNav').append(loginButton);
+
+					$("#loginButton").click(function() {
+						window.location.href = 'login.html';
+					});
+				});
